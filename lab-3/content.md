16d69a6afb12d6e459401887efaf5dd180ea6a4d
dfolomkin <d.folomkin85@gmail.com>
2019-05-06 10:16:02 +0300

    lab-3

---
+++ lab-3/content.md
@@ -0,0 +1,5 @@
+html
+
+js
+modules
+connecting scripts
---
+++ lab-3/server/index.js
@@ -0,0 +1,15 @@
+var jsonServer = require('json-server');
+
+var server = jsonServer.create();
+var router = jsonServer.router('./db.json');
+var middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use('/api', router);
+
+var port = 3000;
+
+server.listen(port, () => {
+  console.log('JSON Server is running on port', port);
+});
